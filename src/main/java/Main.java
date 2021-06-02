d3bc198994420db34360979c3259452dbfca1359
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-02 23:23:40 +0200

    added serviceCaller() method to main class created map from api endpoints and there class types in main() method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,33 @@
+import service.GsonCreator;
+import service.HttpDataCollector;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
+        Map<String, String> endPoints = new HashMap<>();
+        endPoints.put("https://my.api.mockaroo.com/listing?key=63304c70", "Listing");
+        endPoints.put("https://my.api.mockaroo.com/listingStatus?key=63304c70", "ListingStatus");
+        endPoints.put("https://my.api.mockaroo.com/location?key=63304c70", "Location");
+        endPoints.put("https://my.api.mockaroo.com/marketplace?key=63304c70", "Marketplace");
 
-
+        serviceCaller(endPoints);
 
     }
+
+    public static void serviceCaller(Map<String, String> endpoints) throws IOException, ClassNotFoundException {
+
+        for (Map.Entry<String, String> entry : endpoints.entrySet()) {
+            GsonCreator.modelListCreator(
+                    HttpDataCollector.getDataFromApiEndPoint(entry.getKey()), entry.getValue()
+            );
+        }
+    }
+
 }
