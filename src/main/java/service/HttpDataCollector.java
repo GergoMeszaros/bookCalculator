43b1a149b941bdc38f1cfd189c64daca4ddbe0b7
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-02 23:14:16 +0200

    added getDataFromApiEndPoint() method to HttpDataCollector class

--- src/main/java/service/HttpDataCollector.java
+++ src/main/java/service/HttpDataCollector.java
@@ -1,8 +1,33 @@
 package service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class HttpDataCollector {
 
 
+    public static StringBuffer getDataFromApiEndPoint(String apiEndPoint) throws IOException {
 
+        URL url = new URL(apiEndPoint);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
 
+        BufferedReader input = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+
+        while ((inputLine = input.readLine()) != null) {
+            content.append(inputLine );
+        }
+
+        input.close();
+        connection.disconnect();
+
+        return content;
+    }
 }
