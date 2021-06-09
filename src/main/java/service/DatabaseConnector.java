c82e6123078573131a82ffac60d234efe66dea24
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-09 12:12:26 +0200

    DatabaseConnector got private constructor, getDbConnectorInstance() method as it's singleton now. Got createConnection() method which returns a Connection object.

--- src/main/java/service/DatabaseConnector.java
+++ src/main/java/service/DatabaseConnector.java
@@ -1,4 +1,37 @@
 package service;
 
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+import service.config.ReadConfigFile;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnector {
+
+    private static DatabaseConnector databaseConnector;
+    private final ReadConfigFile readConfigFile = new ReadConfigFile();
+
+    private DatabaseConnector() throws IOException {
+    }
+
+    public static DatabaseConnector getDbConnectorInstance() throws IOException {
+        if (databaseConnector == null) {
+            databaseConnector = new DatabaseConnector();
+        }
+        return databaseConnector;
+    }
+
+
+    public Connection createConnection() throws SQLException, IOException {
+
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL(readConfigFile.getDbUrl());
+        dataSource.setUser(readConfigFile.getDbUsername());
+        dataSource.setPassword(readConfigFile.getDbPassword());
+        return dataSource.getConnection();
+    }
 }
