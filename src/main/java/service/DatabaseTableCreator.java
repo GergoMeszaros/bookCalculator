5ffecf2fcc9b51ea4cdb3f9135fe27dc4b8163dc
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-09 13:21:06 +0200

    Added createListingStatus statement to DatabaseTableCreator

--- src/main/java/service/DatabaseTableCreator.java
+++ src/main/java/service/DatabaseTableCreator.java
@@ -17,29 +17,35 @@ public class DatabaseTableCreator {
         Connection connection = databaseConnector.createConnection();
         Statement statement = connection.createStatement();
 
-        String createListing = "" +
+        String createListing =
                 "DROP TABLE IF EXISTS listing; " +
-                "CREATE TABLE listing (" +
-                "   id binary(16) NOT NULL," +
-                "   title varchar(35) NOT NULL," +
-                "   description varchar(255) NOT NULL," +
-                "   inventory_item_location_id binary(16) NOT NULL," +
-                "   listing_price decimal(65, 2) NOT NULL," +
-                "   currency varchar(3) not null," +
-                "   quantity int NOT NULL," +
-                "   listing_status int NOT NULL," +
-                "   marketplace int NOT NULL," +
-                "   upload_time date NOT NULL," +
-                "   owner_email_address varchar(50) NOT NULL," +
-                "   PRIMARY KEY (id)," +
-                "   FOREIGN KEY (inventory_item_location_id) REFERENCES location(id)," +
-                "   FOREIGN KEY (marketplace) REFERENCES marketplace(id)," +
-                "   FOREIGN KEY (listing_status) REFERENCES listing_status(id))";
+                        "CREATE TABLE listing (" +
+                        "   id binary(16) NOT NULL," +
+                        "   title varchar(35) NOT NULL," +
+                        "   description varchar(255) NOT NULL," +
+                        "   inventory_item_location_id binary(16) NOT NULL," +
+                        "   listing_price decimal(65, 2) NOT NULL," +
+                        "   currency varchar(3) not null," +
+                        "   quantity int NOT NULL," +
+                        "   listing_status int NOT NULL," +
+                        "   marketplace int NOT NULL," +
+                        "   upload_time date NOT NULL," +
+                        "   owner_email_address varchar(50) NOT NULL," +
+                        "   PRIMARY KEY (id)," +
+                        "   FOREIGN KEY (inventory_item_location_id) REFERENCES location(id)," +
+                        "   FOREIGN KEY (marketplace) REFERENCES marketplace(id)," +
+                        "   FOREIGN KEY (listing_status) REFERENCES listing_status(id))";
 
         statement.execute(createListing);
 
+        String createListingStatus =
+                "DROP TABLE IF EXISTS listing_status;" +
+                        "CREATE TABLE listing_status (" +
+                        "id int NOT NULL," +
+                        "status_name varchar(35)," +
+                        "PRIMARY KEY (id))";
 
-
+        statement.execute(createListingStatus);
 
     }
 
