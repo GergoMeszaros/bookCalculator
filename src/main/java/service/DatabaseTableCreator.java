30fae9f28a3b04d833d4ed3dbaa4d1bdb46e5ea9
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-09 14:46:20 +0200

    DatabaseTableCreator's DatabaseConnection field replaced with Connection field. Added constructor parameter: connection added tableDropper() method

--- src/main/java/service/DatabaseTableCreator.java
+++ src/main/java/service/DatabaseTableCreator.java
@@ -7,41 +7,32 @@ import java.sql.Statement;
 
 public class DatabaseTableCreator {
 
-    private final DatabaseConnector databaseConnector;
+    private final Connection connection;
 
-    public DatabaseTableCreator() throws IOException {
-        this.databaseConnector = DatabaseConnector.getDbConnectorInstance();
+    public DatabaseTableCreator() throws IOException, SQLException {
+        DatabaseConnector databaseConnector = DatabaseConnector.getDbConnectorInstance();
+        this.connection = databaseConnector.createConnection();
     }
 
-    private void createTable() throws SQLException, IOException {
+    private void tableDropper() throws SQLException {
 
-        Connection connection = databaseConnector.createConnection();
+        Statement dropperStatement = connection.createStatement();
+
+        String tableDropper =
+                "SET foreign_key_checks = 0;" +
+                        "DROP TABLE IF EXISTS listing,listing_status,marketplace, location;" +
+                        "SET foreign_key_checks = 1";
+
+        dropperStatement.execute(tableDropper);
+    }
+
+    public void createTable() throws SQLException {
+        tableDropper();
+
         Statement statement = connection.createStatement();
 
-        String createListing =
-                "DROP TABLE IF EXISTS listing; " +
-                        "CREATE TABLE listing (" +
-                        "   id binary(16) NOT NULL," +
-                        "   title varchar(35) NOT NULL," +
-                        "   description varchar(255) NOT NULL," +
-                        "   inventory_item_location_id binary(16) NOT NULL," +
-                        "   listing_price decimal(65, 2) NOT NULL," +
-                        "   currency varchar(3) not null," +
-                        "   quantity int NOT NULL," +
-                        "   listing_status int NOT NULL," +
-                        "   marketplace int NOT NULL," +
-                        "   upload_time date NOT NULL," +
-                        "   owner_email_address varchar(50) NOT NULL," +
-                        "   PRIMARY KEY (id)," +
-                        "   FOREIGN KEY (inventory_item_location_id) REFERENCES location(id)," +
-                        "   FOREIGN KEY (marketplace) REFERENCES marketplace(id)," +
-                        "   FOREIGN KEY (listing_status) REFERENCES listing_status(id))";
-
-        statement.execute(createListing);
-
         String createListingStatus =
-                "DROP TABLE IF EXISTS listing_status;" +
-                        "CREATE TABLE listing_status (" +
+                "CREATE TABLE listing_status (" +
                         "id int NOT NULL," +
                         "status_name varchar(35)," +
                         "PRIMARY KEY (id))";
@@ -49,8 +40,7 @@ public class DatabaseTableCreator {
         statement.execute(createListingStatus);
 
         String createLocation =
-                "DROP TABLE IF EXISTS location;" +
-                        "CREATE TABLE location (" +
+                "CREATE TABLE location (" +
                         "id binary(16) NOT NULL," +
                         "manager_name varchar(255) NOT NULL," +
                         "phone varchar(15) NOT NULL," +
@@ -64,13 +54,35 @@ public class DatabaseTableCreator {
         statement.execute(createLocation);
 
         String createMarketplace =
-                "DROP TABLE IF EXISTS marketplace;" +
-                        "CREATE TABLE marketplace (" +
+                "CREATE TABLE marketplace (" +
                         "id int NOT NULL," +
                         "marketplace_name varchar(255)," +
                         "PRIMARY KEY (id))";
 
         statement.execute(createMarketplace);
+
+        String createListing =
+                "CREATE TABLE listing (" +
+                        "   id binary(16) NOT NULL," +
+                        "   title varchar(35) NOT NULL," +
+                        "   description varchar(255) NOT NULL," +
+                        "   inventory_item_location_id binary(16) NOT NULL," +
+                        "   listing_price decimal(65, 2) NOT NULL," +
+                        "   currency varchar(3) not null," +
+                        "   quantity int NOT NULL," +
+                        "   listing_status int NOT NULL," +
+                        "   marketplace int NOT NULL," +
+                        "   upload_time date NOT NULL," +
+                        "   owner_email_address varchar(50) NOT NULL," +
+                        "   PRIMARY KEY (id), " +
+                        "   CONSTRAINT location_fk " +
+                        "       FOREIGN KEY (inventory_item_location_id) REFERENCES location(id), " +
+                        "   CONSTRAINT marketplace_fk" +
+                        "       FOREIGN KEY (marketplace) REFERENCES marketplace(id)," +
+                        "   CONSTRAINT status_fk" +
+                        "       FOREIGN KEY (listing_status) REFERENCES listing_status(id))";
+
+        statement.execute(createListing);
     }
 
 }
