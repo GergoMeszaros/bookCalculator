72db07bd61b6b0570e713e12493b45a904c352db
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-09 19:13:40 +0200

    added convertToCsv() method to CsvCreator

--- src/main/java/service/CsvCreator.java
+++ src/main/java/service/CsvCreator.java
@@ -1,4 +1,12 @@
 package service;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CsvCreator {
+
+
+    public String convertToCsv(String [] data){
+        return String.join(",", data);
+    }
 }
