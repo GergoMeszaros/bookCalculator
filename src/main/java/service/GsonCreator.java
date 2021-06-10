4c2193fdcef897a68db39b16f01c267ee47be1f4
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-10 16:44:23 +0200

    GsonCreator has two new methods: collectDataFromApi() and callDatabaseInserter()

--- src/main/java/service/GsonCreator.java
+++ src/main/java/service/GsonCreator.java
@@ -3,39 +3,53 @@ package service;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import model.*;
+import service.database.DatabaseConnector;
+import service.database.DatabaseInserter;
 import service.validator.ApiDataValidator;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.Arrays;
 
 public class GsonCreator {
 
     private static ApiDataValidator apiDataValidator;
+    private static HttpDataCollector dataCollector;
+    private static String apiResponse;
+    private static DatabaseInserter databaseInserter;
 
 
-    public static <T> T[] modelListCreator(String input, Class<?> className) throws ClassNotFoundException, FileNotFoundException {
+    private static void collectDataFromApi(String apiEndPoint) throws IOException {
+        dataCollector = HttpDataCollector.getHttpDataCollectorInstance();
+        apiResponse = dataCollector.getDataFromApiEndPoint(apiEndPoint);
+    }
+
+    public static <T> T[] modelListCreator(String apiEndPoint, Class<?> className) throws ClassNotFoundException, IOException {
+
+        collectDataFromApi(apiEndPoint);
+
 
         GsonBuilder gsonBuilder = new GsonBuilder();
         Gson gson = gsonBuilder
                 .setDateFormat("MM/dd/yyyy")
                 .create();
 
-
       /*  Type userListType = new TypeToken<ArrayList<?>>() {}.getType();
         ArrayList<?> userArray = gson.fromJson(input, userListType);
 
         System.out.println(userArray.get(0));
         */
-        T[] listing = gson.fromJson(input, (Type) className.arrayType());
+        T[] listing = gson.fromJson(apiResponse, (Type) className.arrayType());
 
         if (listing[0] instanceof Listing) {
             apiDataValidator = ApiDataValidator.getApiDataValidatorInstance();
             apiDataValidator.validateApiData((Listing[]) listing);
         }
-        for (T t : listing) {
-            System.out.println(t);
-        }
-
         return listing;
+    }
+
+    private void callDatabaseInserter(Class<?> classname){
+        databaseInserter = databaseInserter.getDatabaseInserterInstance();
     }
 }
