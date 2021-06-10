3bb24386e0097cf84ca0b24d3b749233b7c997e3
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-10 16:43:08 +0200

    DatabaseInserter is singleton, has private constructor and getDatabaseInserterInstance() method

--- src/main/java/service/database/DatabaseInserter.java
+++ src/main/java/service/database/DatabaseInserter.java
@@ -1,4 +1,22 @@
 package service.database;
 
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+
 public class DatabaseInserter {
+
+    private Connection connection;
+    private static DatabaseInserter databaseInserter;
+
+    private DatabaseInserter(){};
+
+    public DatabaseInserter getDatabaseInserterInstance(){
+        if(databaseInserter == null){
+            databaseInserter = new DatabaseInserter();
+        }
+        return databaseInserter;
+    }
+
+
+
 }
