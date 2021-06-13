4b8cfcdd2019816da7d19a6877458dd06e5e0691
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-13 16:57:14 +0200

    modified "set foreign_key_check" query in DatabaseTableCreator

--- src/main/java/service/database/DatabaseTableCreator.java
+++ src/main/java/service/database/DatabaseTableCreator.java
@@ -26,10 +26,10 @@ public class DatabaseTableCreator {
 
         Statement dropperStatement = connection.createStatement();
 
-        String tableDropper =
+        String tableDropper = "" +
                 "SET foreign_key_checks = 0;" +
-                        "DROP TABLE IF EXISTS listing,listing_status,marketplace, location;" +
-                        "SET foreign_key_checks = 1";
+                "DROP TABLE IF EXISTS listing,listing_status,marketplace, location;" +
+                "SET foreign_key_checks = 1";
 
         dropperStatement.execute(tableDropper);
     }
@@ -72,17 +72,17 @@ public class DatabaseTableCreator {
         String createListing =
                 "CREATE TABLE listing (" +
                         "   id varchar(36) NOT NULL," +
-                        "   title varchar(35) NOT NULL," +
+                        "   title varchar(255) NOT NULL," +
                         "   description varchar(255) NOT NULL," +
                         "   location_id varchar(36) NOT NULL," +
                         "   listing_price decimal(65, 2) NOT NULL," +
                         "   currency varchar(3) not null," +
                         "   quantity int NOT NULL," +
-                        "   listing_status int NOT NULL," +
+                        "   listing_status int," +
                         "   marketplace int NOT NULL," +
-                        "   upload_time date NOT NULL," +
+                        "   upload_time date," +
                         "   owner_email_address varchar(50) NOT NULL," +
-                        "   PRIMARY KEY (id), " +
+                        "   PRIMARY KEY (id)," +
                         "   CONSTRAINT location_fk " +
                         "       FOREIGN KEY (location_id) REFERENCES location(id), " +
                         "   CONSTRAINT marketplace_fk" +
@@ -92,5 +92,4 @@ public class DatabaseTableCreator {
 
         statement.execute(createListing);
     }
-
 }
