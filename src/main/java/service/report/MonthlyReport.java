4261dddee894e6eb0cfc2a2484873b635680674f
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-13 17:21:16 +0200

    Report renamed to MonthlyReport and fields added

---
+++ src/main/java/service/report/MonthlyReport.java
@@ -0,0 +1,13 @@
+package service.report;
+
+import model.MarketPlaceType;
+
+public class MonthlyReport {
+
+    private MarketPlaceType marketPlaceType;
+    private String bestListerEmailAddress;
+    private Long totalListingPriceOfMonth;
+    private Long totalListingCountOfMonth;
+
+
+}
--- src/main/java/service/report/Report.java
+++
@@ -1,6 +0,0 @@
-package service.report;
-
-public class Report {
-
-
-}
