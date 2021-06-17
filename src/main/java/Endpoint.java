c82cf5b4318dba5f17e4b0d3b53c60ee998f1cac
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-17 10:15:18 +0200

    created Endpoint class with private createMap() method This class collects all the necessary api endpoints

---
+++ src/main/java/Endpoint.java
@@ -0,0 +1,33 @@
+import model.Listing;
+import model.ListingStatus;
+import model.Location;
+import model.Marketplace;
+import service.config.ReadConfigFile;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Endpoint {
+
+    private Map<String, Class<?>> endpoints;
+    private final ReadConfigFile readConfigFile;
+
+    public Endpoint(ReadConfigFile readConfigFile) {
+        this.readConfigFile = readConfigFile;
+        createMap();
+    }
+
+    private void createMap() {
+        endpoints = new TreeMap<>(Collections.reverseOrder());
+
+        endpoints.put(readConfigFile.getListing(), Listing.class);
+        endpoints.put(readConfigFile.getListingStatus(), ListingStatus.class);
+        endpoints.put(readConfigFile.getMarketplace(), Marketplace.class);
+        endpoints.put(readConfigFile.getLocation(), Location.class);
+    }
+
+    public Map<String, Class<?>> getEndpoints() {
+        return endpoints;
+    }
+}
