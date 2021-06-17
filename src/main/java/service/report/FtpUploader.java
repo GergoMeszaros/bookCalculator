b171639d6a76118ce7c11f04ff09cec9d7014230
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-17 10:12:34 +0200

    created FtpUploader with it's fields and public upload() method

---
+++ src/main/java/service/report/FtpUploader.java
@@ -0,0 +1,65 @@
+package service.report;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import service.config.ReadConfigFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FtpUploader {
+
+    private final String ftpAddress;
+    private final String ftpUsername;
+    private final String ftpPassword;
+    private final String reportFileName;
+
+    public FtpUploader(ReadConfigFile readConfigFile) {
+        ftpAddress = readConfigFile.getFtpServer();
+        ftpUsername = readConfigFile.getFtpUsername();
+        ftpPassword = readConfigFile.getFtpPassword();
+        reportFileName = readConfigFile.getReportFileName();
+    }
+
+    public void upload() {
+
+        FTPClient ftpClient = new FTPClient();
+
+        try {
+            ftpClient.connect(ftpAddress);
+            ftpClient.login(ftpUsername, ftpPassword);
+            ftpClient.enterLocalPassiveMode();
+
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+
+            File file = new File(reportFileName);
+            String remoteFileName = "jsonreport.json";
+
+            InputStream inputStream = new FileInputStream(file);
+            System.out.println("Uploading file");
+            boolean done = ftpClient.storeFile(remoteFileName, inputStream);
+            inputStream.close();
+
+            if (done) {
+                System.out.println("File successfully uploaded");
+            } else {
+                System.out.println("Failed to upload file");
+            }
+
+        } catch (IOException exception) {
+            System.out.println("Failed to connect ftp " + exception.getMessage());
+            exception.printStackTrace();
+        } finally {
+            try {
+                if (ftpClient.isConnected()) {
+                    ftpClient.logout();
+                    ftpClient.disconnect();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
