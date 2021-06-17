935c57fcbf6381737b23a734ed61d323458907c7
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-17 14:40:38 +0200

    removed empty line

--- src/main/java/model/MonthlyReport.java
+++ src/main/java/model/MonthlyReport.java
@@ -34,7 +34,6 @@ public class MonthlyReport {
         return MarketPlaceType.getMarketPlaceTypeNameFromId(marketPlaceId);
     }
 
-
     @Override
     public String toString() {
         return "MonthlyReport{" +
