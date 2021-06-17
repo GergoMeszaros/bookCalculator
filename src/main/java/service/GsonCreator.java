fabcadbeeb2fe6ca78d1024793f7bf346bd731d4
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-17 10:11:44 +0200

    DatabaseInserter and HttpDataCollector injected into GsonCreator via it's constructor

--- src/main/java/service/GsonCreator.java
+++ src/main/java/service/GsonCreator.java
@@ -3,54 +3,49 @@ package service;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import model.*;
-import service.database.DatabaseConnector;
 import service.database.DatabaseInserter;
 import service.validator.ApiDataValidator;
+import service.validator.HttpDataCollector;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.sql.SQLException;
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.util.Arrays;
-import java.util.Locale;
 
 public class GsonCreator {
 
-    private static ApiDataValidator apiDataValidator;
-    private static HttpDataCollector dataCollector;
-    private static String apiResponse;
-    private static DatabaseInserter databaseInserter;
+    private final ApiDataValidator apiDataValidator;
+    private final HttpDataCollector dataCollector;
+    private String apiResponse;
+    private final DatabaseInserter databaseInserter;
 
+    public GsonCreator(ApiDataValidator apiDataValidator, HttpDataCollector dataCollector, DatabaseInserter databaseInserter) {
+        this.apiDataValidator = apiDataValidator;
+        this.dataCollector = dataCollector;
+        this.databaseInserter = databaseInserter;
+    }
 
-    private static void collectDataFromApi(String apiEndPoint) throws IOException {
-        dataCollector = HttpDataCollector.getHttpDataCollectorInstance();
+    private void collectDataFromApi(String apiEndPoint) throws IOException {
         apiResponse = dataCollector.getDataFromApiEndPoint(apiEndPoint);
     }
 
-    public static <T> void  modelListCreator(String apiEndPoint, Class<?> className) throws ClassNotFoundException, IOException, SQLException, ParseException {
+    public <T> void modelListCreator(String apiEndPoint, Class<?> className) throws IOException, SQLException, ParseException {
 
         collectDataFromApi(apiEndPoint);
 
         GsonBuilder gsonBuilder = new GsonBuilder();
-        Gson gson = gsonBuilder
-                .create();
+        Gson gson = gsonBuilder.create();
 
         T[] listing = gson.fromJson(apiResponse, (Type) className.arrayType());
 
         if (listing[0] instanceof Listing) {
-            apiDataValidator = ApiDataValidator.getApiDataValidatorInstance();
             apiDataValidator.validateApiData((Listing[]) listing);
             callDatabaseInserter(className, listing);
         }
-
         callDatabaseInserter(className, listing);
     }
 
-    private static <T> void callDatabaseInserter(Class<?> classname, T[] arrayOfModels) throws SQLException, IOException, ParseException {
-        databaseInserter = DatabaseInserter.getDatabaseInserterInstance();
+    private <T> void callDatabaseInserter(Class<?> classname, T[] arrayOfModels) throws SQLException, IOException, ParseException {
         databaseInserter.forwardDataToAppropriateInserter(arrayOfModels, classname);
-
     }
 }
