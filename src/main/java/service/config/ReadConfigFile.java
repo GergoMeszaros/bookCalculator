eba23f13b284e2ca5c72491b32c719a01a66044c
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-17 10:02:22 +0200

    Added new fields to ReadConfigFile: reportFileName;     String csvName;     String ftpUsername;     String ftpPassword;     String ftpServer;     String listing;     String listingStatus;     String marketplace;     String location;

--- src/main/java/service/config/ReadConfigFile.java
+++ src/main/java/service/config/ReadConfigFile.java
@@ -6,12 +6,20 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class ReadConfigFile {
+
     InputStream inputStream;
     String dbUsername;
     String dbPassword;
-    String dbName;
     String dbUrl;
-    String driverName;
+    String reportFileName;
+    String csvName;
+    String ftpUsername;
+    String ftpPassword;
+    String ftpServer;
+    String listing;
+    String listingStatus;
+    String marketplace;
+    String location;
 
     public String getDbUsername() {
         return dbUsername;
@@ -21,16 +29,44 @@ public class ReadConfigFile {
         return dbPassword;
     }
 
-    public String getDbName() {
-        return dbName;
-    }
-
     public String getDbUrl() {
         return dbUrl;
     }
 
-    public String getDriverName() {
-        return driverName;
+    public String getReportFileName() {
+        return reportFileName;
+    }
+
+    public String getCsvName() {
+        return csvName;
+    }
+
+    public String getFtpUsername() {
+        return ftpUsername;
+    }
+
+    public String getFtpPassword() {
+        return ftpPassword;
+    }
+
+    public String getFtpServer() {
+        return ftpServer;
+    }
+
+    public String getListing() {
+        return listing;
+    }
+
+    public String getListingStatus() {
+        return listingStatus;
+    }
+
+    public String getMarketplace() {
+        return marketplace;
+    }
+
+    public String getLocation() {
+        return location;
     }
 
     public ReadConfigFile() throws IOException {
@@ -52,18 +88,24 @@ public class ReadConfigFile {
             }
             dbUsername = properties.getProperty("dbUsername");
             dbPassword = properties.getProperty("dbPassword");
-            dbName = properties.getProperty("dbName");
             dbUrl = properties.getProperty("dbUrl");
-            driverName = properties.getProperty("driverName");
+            reportFileName = properties.getProperty("fileName");
+            csvName = properties.getProperty("csvName");
+            ftpServer = properties.getProperty("ftpServer");
+            ftpUsername = properties.getProperty("ftpUsername");
+            ftpPassword = properties.getProperty("ftpPassword");
+            listing = properties.getProperty("listing");
+            listingStatus = properties.getProperty("listingStatus");
+            marketplace = properties.getProperty("marketplace");
+            location = properties.getProperty("location");
 
         } catch (IOException exception) {
-            System.out.println("Exception " + exception);
+            exception.printStackTrace();
         } finally {
-            inputStream.close();
+            if (inputStream != null) {
+                inputStream.close();
+            }
         }
-
-
     }
-
 
 }
