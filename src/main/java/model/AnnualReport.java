f8cb65017d80a85722dc2b68042436094f39dcd4
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-17 09:53:44 +0200

    added toString() to annualReport

--- src/main/java/model/AnnualReport.java
+++ src/main/java/model/AnnualReport.java
@@ -2,11 +2,27 @@ package model;
 
 public class AnnualReport extends MonthlyReport {
 
-    private Long totalListingCount;
 
-
-    public AnnualReport(MarketPlaceType marketPlaceType, String bestListerEmailAddress, Long totalListingPriceOfMonth, Long totalListingCountOfMonth, Long totalListingCount) {
-        super(marketPlaceType, bestListerEmailAddress, totalListingPriceOfMonth, totalListingCountOfMonth);
+    public AnnualReport(int year, int marketPlaceType, String bestListerEmailAddress, Float totalListingPrice, Long totalListingCount, Float averageListingPrice ){
         this.totalListingCount = totalListingCount;
+        this.year = year;
+        this.marketPlaceType = setMarketPlaceType(marketPlaceType);
+        this.bestListerEmailAddress = bestListerEmailAddress;
+        this.totalListingPrice = totalListingPrice;
+        this.averageListingPrice = averageListingPrice;
     }
+
+    @Override
+    public String toString() {
+        return "AnnualReport{" +
+                "year=" + year +
+                ", marketPlaceType='" + marketPlaceType + '\'' +
+                ", bestListerEmailAddress='" + bestListerEmailAddress + '\'' +
+                ", totalListingPrice=" + totalListingPrice +
+                ", totalListingCount=" + totalListingCount +
+
+                '}';
+    }
+
+
 }
