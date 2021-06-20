e12f0cff61571cec880a7b0a74ef1c16b6ed264d
GergoMeszaros <gergo.meszaros.codecool@gmail.com>
2021-06-20 09:59:19 +0200

    added returnConnection test to DatabaseConnectorTest

--- src/test/java/service/GsonCreatorTest.java
+++ src/test/java/service/GsonCreatorTest.java
@@ -2,14 +2,22 @@ package service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import model.Listing;
+import model.ListingStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import service.validator.ApiDataValidator;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 class GsonCreatorTest {
+
 
 
     @BeforeEach
@@ -24,8 +32,4 @@ class GsonCreatorTest {
 
         assertNotNull(gson);
     }
-
-
-
-
 }
--- src/test/java/service/database/DatabaseConnectorTest.java
+++ src/test/java/service/database/DatabaseConnectorTest.java
@@ -11,16 +11,19 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class DatabaseConnectorTest {
 
     private MysqlDataSource dataSource;
+    private DatabaseConnector databaseConnector;
     Connection connection;
 
 
     @BeforeEach
     void setup() {
         dataSource = Mockito.mock(MysqlDataSource.class);
+        databaseConnector = Mockito.mock(DatabaseConnector.class);
     }
 
     @Test
@@ -36,5 +39,10 @@ class DatabaseConnectorTest {
             exception.printStackTrace();
         }
         assertNull(connection);
+    }
+
+    @Test
+    void returnConnection() throws SQLException {
+        when(databaseConnector.createConnection()).thenReturn(connection);
     }
 }
